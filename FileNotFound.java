fb34a025bc0a1263f56d6320b63f9a1cfa9730c2
SahalBasheer <76899304+SahalBasheer@users.noreply.github.com>
2022-02-20 22:20:47 +0530

    Create FileNotFound.java

---
+++ FileNotFound.java
@@ -0,0 +1,16 @@
+//1. Write a program that demonstrates exception handling mechanism.
+//(c ) File not found exception
+
+package Cycle6;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilenotFound {
+    public static void main(String args[]) throws IOException {
+    try{
+        FileReader fin = new FileReader("input.txt");
+    }catch(FileNotFoundException e){System.out.println("File not Found in directory");} 
+    }
+    
+}
