ca0894a15a3e779384c7fb389c8d65274e7818bf
SahalBasheer <76899304+SahalBasheer@users.noreply.github.com>
2022-02-20 22:18:30 +0530

    Create ArrayOutofBound.java

---
+++ ArrayOutofBound.java
@@ -0,0 +1,23 @@
+//1. Write a program that demonstrates exception handling mechanism.
+//(b) Array out of index exception
+
+
+package Cycle6;
+
+import java.util.Scanner;
+
+public class ArrayOutofBound {
+    public static void main(String args[]) {
+        Scanner sc =  new Scanner(System.in); 
+        int array[] = new int[3];
+        System.out.printf("Enter the Elemnts of the array : " );
+        try{
+            for(int i =0; i<=array.length; i++)
+            {
+               array[i] = sc.nextInt();
+            }
+        }catch(ArrayIndexOutOfBoundsException e){System.out.println("Array out of Memory");}
+        
+    }
+    
+}
